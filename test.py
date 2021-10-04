63af8b33e469cee07bb185f0121ce00c7119da4b
Bence Gavallér <gavallerb15@gmail.com>
2021-10-04 21:51:01 +0200

    csatlakozas teszteles

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ conn=http.client.HTTPSConnection("v3.football.api-sports.io")
 
 headers={
     'x-rapidapi-host': "v3.football.api-sports.io",
-    'x-apisports-key': "e867d6bae65940063bb5037d1cd95063"
+    'x-apisports-key': ""
 }
 
-conn.request("GET","/fixtures",headers=headers)
+conn.request("GET","/leagues",headers=headers)
 
 res=conn.getresponse()
 data=res.read()
